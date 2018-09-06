2d5e25134cd546d7a452cf7cdb1270fb8f485bdf
Daniel Bamidele <greatbolutife@gmail.com>
2018-09-06 07:48:47 +0100

    feat(layout): setup navbar and footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <footer class="footer">
+      <div class="container">
+        <nav>
+          <ul>
+            <li>
+              <router-link to="/" class="nav-link">
+                  NEWS NINJA
+              </router-link>
+            </li>
+            <li>
+              <a href="#" class="nav-link">
+                About Us
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div class="copyright">
+          &copy; {{getYear}}, Theme designed by <a href="https://www.creative-tim.com/" target="_blank" class="copyright-link">Creative Tim</a>. Coded by <a href="https://github.com/humanityjs" target="_blank" class="copyright-link">Bamidele Daniel</a>.
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    getYear() {
+      return new Date().getFullYear()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  background-color: #000;
+  color: #fff;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg" color-on-scroll="500">
+      <div class="container">
+        <div class="navbar-translate">
+          <router-link to="/" class="navbar-brand">
+            News Ninja
+          </router-link>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-bar top-bar"></span>
+            <span class="navbar-toggler-bar middle-bar"></span>
+            <span class="navbar-toggler-bar bottom-bar"></span>
+          </button>
+        </div>
+        <div class="collapse navbar-collapse show" data-nav-image="./img/blurred-image-1.jpg" data-color="orange">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <router-link to="/favorites"  class="nav-link">
+                <p>Favorites</p>
+              </router-link >
+            </li>
+            <!-- <li class="nav-item dropdown">
+              <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown">
+                <i class="now-ui-icons design_image" aria-hidden="true"></i>
+                <p>Examples</p>
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="examples/about-us.html">
+                  <i class="now-ui-icons business_bulb-63"></i>
+                  About-us
+                </a>
+                <a class="dropdown-item" href="examples/blog-post.html">
+                  <i class="now-ui-icons text_align-left"></i>
+                  Blog Post
+                </a>
+              </div>
+            </li> -->
+          </ul>
+          </div>
+      </div>
+    </nav>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss" scoped>
+.navbar {
+  background-color: rgb(97,198,178)
+}
+</style>
